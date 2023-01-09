e8f51f6db184b73fe66254a88e39dd56dc67fe14
VanessaUwase03 <uwasevanny10@gmail.com>
2023-01-09 15:55:36 +0200

    inputs

---
+++ src/components/Inputs/index.tsx
@@ -0,0 +1,44 @@
+import { ChangeEventHandler, useState } from "react";
+import "./index.css"
+
+type Props = {
+    name: string,
+    type?:"email"|"password"|"text"|"number"|"date",
+    variant:"one"|"two"|"three",
+    label:string,
+    helperText?:string,
+    errorText?:string,
+    onChange?: ChangeEventHandler<HTMLInputElement>
+}
+export const Textfield =({
+    name,
+    type = "text",
+    variant = "one", 
+    label,
+    helperText,
+    onChange,
+    errorText
+}:Props) =>{
+    const [inputValue, setInputValue] = useState("");
+    const inputId= name;
+    const emptyInput = inputValue !=="" ? "not empty": "";
+
+    return  <div className={`text-field ${variant} ${emptyInput}`} >
+
+              <input id={inputId} name={name} type={type} onChange={V=>{
+                setInputValue(V.target.value);
+                if(onChange){
+                    
+                }
+              }}/>
+               <label htmlFor={inputId}>{label}</label>
+               {
+            !helperText ? null :
+               <span className="helper-text">{helperText}</span>
+            }
+            {
+                !errorText ? null:
+                <span className="error-text">{errorText}</span>
+            }
+             </div>
+}
